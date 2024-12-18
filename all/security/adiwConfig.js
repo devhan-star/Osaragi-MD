f30905a5cbc19a7ee73333b62f49ea3f91fdf88e
devhan-star <rehanrehan12328@gmail.com>
2024-12-18 21:22:48 +0700

    Update adiwConfig.js
    dah

--- all/security/adiwConfig.js
+++ all/security/adiwConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   'approval': {
-    'num': '6289508082845',
+    'num': '6283845427520',
     'text': "oke",
     'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
     'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
